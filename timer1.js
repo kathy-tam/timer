7bfb3f36fa6abfbded9c4975b209020b8f970aea
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-29 03:21:18 +0000

    Outputs . for every specified time

---
+++ timer1.js
@@ -0,0 +1,12 @@
+//User input as arguments
+//Arguments are strings
+let times = process.argv.slice(2);
+
+//Edge cases
+if(!times.length) { return }  //no args
+times = times.map(time => Number(time)).filter(time => time >= 0 && typeof time === 'number'); //not a number or -number 
+times.sort((a,b) => a - b);
+
+for(const time of times) {
+  setTimeout(() => process.stdout.write('.'), time*1000);
+}
