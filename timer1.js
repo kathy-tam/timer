a96862d08cc54713638e0046bdeeed910bd06f82
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-29 03:22:05 +0000

    Added comment that beep isn't working

--- timer1.js
+++ timer1.js
@@ -8,5 +8,6 @@ times = times.map(time => Number(time)).filter(time => time >= 0 && typeof time 
 times.sort((a,b) => a - b);
 
 for(const time of times) {
+  //beep: '\x07' not working
   setTimeout(() => process.stdout.write('.'), time*1000);
 }
